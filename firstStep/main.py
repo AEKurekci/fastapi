3d3a7e627aa9295a3638a4bed9861c2d3a630793
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2020-05-22 16:00:07 +0300

    Enum

--- firstStep/main.py
+++ firstStep/main.py
@@ -1,21 +1,40 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 class Item(BaseModel):
     name: str
     price: float
     isOffer: bool = None
 
+
 @app.get("/")
-def readRoot():
+async def root():
     return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def readItem(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.put("/items/{item_id}")
 def updateItem(item_id: int, item: Item):
     return {"item_name:": item.name, "item_id": item_id}
+
+
+@app.get("/model/{modelName}")
+async def getModel(modelName: ModelName):
+    if modelName == ModelName.alexnet:
+        return {"model_name ": modelName, "message ": "Machine Learning FTW!"}
+    if modelName.value == ModelName.lenet:
+        return {"model_name ": modelName, "message ": "LeCNN all the images"}
+    return {"model_name ": modelName, "message ": "Have some residuals"}
